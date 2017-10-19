2ab9e9cd3e9878bef9791a516c1678e2298564a3
Jörn Viebrock <joernviebrock+programmers@gmail.com>
2017-10-19 08:54:54 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './App.css';
+
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to "simple listing filter" example</h2>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
