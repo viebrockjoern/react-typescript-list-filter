e088fbb98f598daf895da36a64560779788b6df0
Jörn Viebrock <joernviebrock+programmers@gmail.com>
2017-10-19 09:44:35 +0200

    Configured the tslint.json for allowing console.log. Adding an a-z filter for the cities list. Over the state class object and the onChange event on the select tag, these components are connected

--- src/App.tsx
+++ src/App.tsx
@@ -17,33 +17,77 @@ const cities = [
     {'title': 'Hannover', 'population': 532163}
 ];
 
-
 class App extends React.Component {
 
+    public state: {letter: string};
+    private alphabet: string[];
+
+    constructor(props: {}) {
+
+        super(props);
+        this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+        this.state = {
+            letter: ''
+        };
+    }
+
+    letterChange = (e: any) => {
+
+        let letter = e.target.value;
+
+        this.setState({letter: letter});
+    }
+
     render() {
+
+        let currentLetter = this.state.letter;
+
         return (
           <div className="App">
             <div className="App-header">
               <h2>simple listing filter</h2>
             </div>
 
+              <div className="filters">
+
+                  <div className="filter letter">
+                      City starts with
+                      <select onChange={this.letterChange}>
+                          <option value="">-</option>
+                          {this.alphabet.map(function(letter: string, key: number) {
+
+                              return (
+                                  <option key={key} value={letter}>{letter}</option>
+                              );
+                          })}
+                      </select>
+                  </div>
+
+              </div>
 
               <div className="listing">
-                  {cities.map(function(city: any) {
+                  {cities.map(function(city: {title: string, population: number}, key: number) {
 
-                      return (
-                          <div className="entry">
-                              <div className="title">{city.title}</div>
-                              <div className="population">{city.population}</div>
-                          </div>
-                      );
+                        let firstLetter: string = city.title.substr(0, 1).toLowerCase();
+                        if (currentLetter === '' || currentLetter === firstLetter) {
+
+                            return (
+                                <div key={key} className="entry">
+                                    <div className="title">{city.title}</div>
+                                    <div className="population">{city.population}</div>
+                                </div>
+                            );
+                        } else {
+                            return '';
+                        }
+
                   })}
               </div>
 
           </div>
         );
     }
-
 
 }
 
